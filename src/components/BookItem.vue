498c920e523f518aed607c136a30be78ae52b31d
noecl1 <noecl1@hotmail.com>
2020-01-26 12:24:50 -0700

    Connecting VookItem as a Child Component of BookList

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,16 +1,15 @@
 <template>
-  <li>{{book.title}}:{{book.author}}</li>
+  <li>{{ book.title }}:{{ book.author }}</li>
 </template>
 
 <script>
 export default {
-  name: 'BookItem',
-  props: ['book']
-}
+  name: "BookItem",
+  props: ["book"]
+};
 </script>
 
 <style>
-
 li {
   display: block;
   margin: 0 10px;
